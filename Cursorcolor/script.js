814af194e90936222b3134633e469fdc3e3f2889
Kongriel <124664554+Kongriel@users.noreply.github.com>
2024-02-15 22:31:31 +0100

    Update script.js

--- Cursorcolor/script.js
+++ Cursorcolor/script.js
@@ -3,13 +3,13 @@ const body = document.getElementById("body");
 document.addEventListener("mousemove", (e) => {
   const xPercent = (e.clientX / window.innerWidth) * 100;
 
-  // Map xPercent to a range of lightness values (0 to 100)
+
   const lightness = xPercent;
 
-  // Calculate hue based on the y position (vertical position)
+  // y
   const yPercent = (e.clientY / window.innerHeight) * 100;
-  const hue = (yPercent / 100) * 360; // Map yPercent to a range of hues (0 to 360)
-
+  const hue = (yPercent / 100) * 360; 
+  
   // baggrundsfarve
   body.style.backgroundColor = `hsl(${hue}, 100%, ${lightness}%)`;
 });
